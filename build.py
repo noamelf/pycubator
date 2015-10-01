352b638552e90e7b54bd3468499f5b2b10cdad3b
Noam Elfanbaum <noamelf@gmail.com>
2015-10-02 01:54:49 +0300

    Ready for show time

--- build.py
+++ build.py
@@ -1,6 +1,9 @@
 from pathlib import Path
-from jinja2 import Environment, FileSystemLoader
 import sys
+
+from jinja2 import Environment, FileSystemLoader
+import os
+import shutil
 
 get_tmpl = Environment(loader=FileSystemLoader('templates/')).get_template
 
@@ -13,7 +16,7 @@ def extract_slide_title(md_file):
 
 
 def get_md_files():
-    p = Path('content/')
+    p = Path('output', 'content')
     for md_file in sorted(p.iterdir()):
         if md_file.suffix == '.md':
             yield md_file, extract_slide_title(md_file)
@@ -22,17 +25,17 @@ def get_md_files():
 def main():
     slide_tmpl, index_tmpl = get_tmpl('slide.j2'), get_tmpl('index.j2')
 
-    md_files = [(str(md_file), 'output/' + md_file.with_suffix('.html').name, title)
+    md_files = [(os.path.join(*md_file.parts[1:]), md_file.with_suffix('.html').name, title)
                 for md_file, title in get_md_files()]
 
     for md_file, html_file, title in md_files:
         rendered_template = slide_tmpl.render(mdfile=md_file, title=title)
-        with open(html_file, 'w') as f:
+
+        with open(os.path.join('output', html_file), 'w') as f:
             f.write(rendered_template)
 
     with open('output/index.html', 'w') as f:
         f.write(index_tmpl.render(slides=md_files))
-
 
 if __name__ == '__main__':
     sys.exit(main())
