802b2f40ccc46fceb73df9b7e1f740a59337df75
Noam Elfanbaum <noamelf@gmail.com>
2015-11-21 22:26:59 +0200

    Added interleave example

--- code-examples/unit-testing/interleave.py
+++ code-examples/unit-testing/interleave.py
@@ -1,6 +1,9 @@
 from itertools import izip_longest
-
+#
+#
+# def interleave(a, b):
+#     """Return the interleaving of two sequences as a list."""
+#     return [y for x in izip_longest(a, b) for y in x if y is not None]
 
 def interleave(a, b):
-    """Return the interleaving of two sequences as a list."""
-    return [y for x in izip_longest(a, b) for y in x if y is not None]
+    return [y for x in izip_longest(a, b) for y in x]
